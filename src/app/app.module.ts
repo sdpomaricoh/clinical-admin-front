817f5c44f809c7d51a48b2380e69733123239c5c
Sergio Pomárico <sergiodavid21@gmail.com>
2018-05-06 11:59:40 -0500

    add panel styles and scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotfoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule
